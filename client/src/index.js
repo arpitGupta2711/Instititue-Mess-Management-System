135a3af20c308b734804fca6bdf790d6854b5416
Arpit <agvinayak27@gmail.com>
2023-04-19 03:00:24 +0530

    revert

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,14 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import {store} from './store'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-
     <Provider store = {store}>
      <App  />  
     </Provider>
-    </GoogleOAuthProvider>;
   </React.StrictMode>
 );
 
--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,73 +1,22 @@
-import React from "react";
-import { useState } from "react";
-import DashboardNavbar from "../components/dashboard-navbar";
-import {
-  Box,
-  Grid,
-  Paper,
-  Typography,
-  Card,
-  CardContent,
-  CardActions,
-  Button,
-  CardMedia,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import "./styles.css";
-import QRCode from "react-qr-code";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-const item = [
+import React from 'react'
+import DashboardNavbar from '../components/dashboard-navbar'
+import {Box,Grid, Paper, Typography,Card,CardContent,CardActions, Button,CardMedia,Accordion,AccordionSummary,AccordionDetails} from '@mui/material'
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+const item=[
   {
-    img: "https://unsplash.com/photos/1Su3t-fqNxY",
-    title: "evening",
-  },
-];
+    img:'https://unsplash.com/photos/1Su3t-fqNxY',
+    title:'evening'
+  }
+]
 const ProfilePage = () => {
-  const [showQr, setShowQr] = useState(false);
-  const [value, setValue] = useState("akahs is chutiya");
   return (
     <>
-      <DashboardNavbar></DashboardNavbar>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Button
-          sx={{ width: "40%" }}
-          variant="contained"
-          onClick={() => {
-            setShowQr(true);
-          }}
-        >
-          Show QR Code
-        </Button>
-      </div>
-
-      {showQr && (
-        <div
-          onClick={() => {
-            setShowQr(false);
-          }}
-          className="overlay"
-        >
-          <div
-            className="form-container"
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-          >
-            {value && <QRCode title="Your account" 
-             size={256}
-             style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-             viewBox={`0 0 256 256`}
-            
-            
-            value={value} />}
-          </div>
-        </div>
-      )}
+     <DashboardNavbar></DashboardNavbar>
+     
+     
     </>
-  );
-};
+  )
+}
 
-export default ProfilePage;
+export default ProfilePage
