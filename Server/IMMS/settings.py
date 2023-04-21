9c99fb5b4ef04d7caee5aaba2853dbd8ccfbe9e6
Akash Singh <akash@pop-os.localdomain>
2023-04-21 16:51:35 +0530

    Postgres Setup

--- Server/IMMS/settings.py
+++ Server/IMMS/settings.py
@@ -135,11 +135,14 @@ WSGI_APPLICATION = 'IMMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'imms',
+        'USER': 'messuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
